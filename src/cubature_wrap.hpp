c706cbdc564f4f84df051a1e8a05afa3b786a7f4
Oleg Skoromnik <olegskor@gmail.com>
2018-05-28 23:56:43 +0200

    implemented first version of wrapper. added .swp in .gitignore

--- src/cubature_wrap.hpp
+++ src/cubature_wrap.hpp
@@ -1,31 +1,121 @@
 #include <cubature.h>
+#include <tuple>
+#include <array>
+#include <cstdlib>
 
 namespace cub{
 
-	template <typename F>
-		class integrand{
+	template<class F>
+		struct function_traits;
+
+	// function pointer
+	template<class R, class... Args>
+		struct function_traits<R(*)(Args...)>
+		: public function_traits<R(Args...)> {};
+
+	template<class R, class... Args>
+		struct function_traits<R(Args...)>
+		{
+			using return_type = R;
+
+			static constexpr std::size_t arity = sizeof...(Args);
+
+			template <std::size_t N>
+				struct argument
+				{
+					static_assert(N < arity,
+							"error: invalid parameter index.");
+					using type =
+						typename std::tuple_element<N,
+								 std::tuple<Args...>>::type;
+				};
+		};
+
+	template <typename F, std::size_t F_SIZE, std::size_t X_SIZE>
+		class integrand_cxx{
+			public:
+				integrand_cxx(F func, std::size_t limit)
+					:func(func),
+					//f_size(std::tuple_size<
+							//typename
+							//function_traits<decltype(func)>::return_type
+							//>::value),
+					//x_size(std::tuple_size<
+							//typename
+							//function_traits<decltype(func)>::return_type
+							//>::value),
+					limit(limit) {}
+
+				std::array<std::array<double,2>,F_SIZE> integrate(
+						const std::array<double,X_SIZE>& x_min,
+						const std::array<double,X_SIZE>& x_max,
+						double eps_abs,
+						double eps_rel,
+						error_norm norm = ERROR_INDIVIDUAL
+						)
+				{
+					integrand i = &cubature_int_wrapper;
+					double *res = (double*)malloc(F_SIZE * sizeof(double));
+					double *err = (double*)malloc(F_SIZE * sizeof(double));
+					if(hcubature(F_SIZE, i, this, X_SIZE, x_min.data(),
+							x_max.data(), limit, eps_abs, eps_rel,
+							norm, res, err)){
+						throw(std::runtime_error(
+									"Error during integration"));
+					}
+					std::array<std::array<double,2>,F_SIZE> res_err;
+					for(std::size_t j = 0; j < F_SIZE; ++j){
+						res_err[j][0] = res[j];
+						res_err[j][1] = err[j];
+					}
+					free(res);
+					free(err);
+
+					return res_err;
+				}
+
 			private:
 				F func;
-				unsigned f_size;
-				unsigned x_size;
-				std::size_t limit;
+				//const unsigned f_size;
+				//const unsigned x_size;
+				const std::size_t limit;
 
-				static int cubature_int(
+				static int cubature_int_wrapper(
 						unsigned ndim,
 						const double *x,
 						void *params,
 						unsigned fdim,
 						double *fval)
 				{
-					integrand *i = reinterpret_cast<intgrand*>(params);
-					std::vector<double> x_arr;
-					for(std::size_t j = 0; j < x_size; ++j){
-						x_arr.push_back(x[i]);
+					integrand_cxx *i = reinterpret_cast<integrand_cxx*>(
+							params);
+					std::array<double, X_SIZE> x_arr;
+					for(std::size_t j = 0; j < X_SIZE; ++j){
+						x_arr.at(j) = x[j];
 					}
-					std::vector<double> f_arr = i->func(x_arr);
-					for(std::size_t j = 0; j < f_size; ++j){}
-					return i->func(x_arr);
+					std::array<double,F_SIZE> f_arr = i->func(x_arr);
+					for(std::size_t j = 0; j < F_SIZE; ++j){
+						fval[j] = f_arr.at(j);
+					}
+					return 0;
 				}
+
+		};
+
+	template <typename F, std::size_t F_SIZE, std::size_t X_SIZE>
+		auto make_cubature_int(
+				F func,
+				std::size_t limit,
+				const std::array<double,X_SIZE>& x_min,
+				const std::array<double,X_SIZE>& x_max,
+				double eps_abs,
+				double eps_rel,
+				error_norm norm = ERROR_INDIVIDUAL
+				)
+		{
+			integrand_cxx<F,F_SIZE,X_SIZE> i(func,limit);
+
+			return i.integrate(x_min, x_max, eps_abs, eps_rel, norm);
 		}
 
 } /* end namespace cub */
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <array>
+#include <cmath>
+#include <type_traits>
+
+#include <cubature_wrap.hpp>
+
+std::array<double,1> f(const std::array<double,3> &x){
+	double e = 0.;
+	for(const auto &el: x){
+		e += el*el;
+	}
+	return {exp(-e)};
+}
 
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello world\n";
+	using traits = cub::function_traits<decltype(f)>;
+	std::cout << "number of arguments = " << traits::arity << "\n";
+	std::cout << std::is_same<traits::argument<0>::type,
+		const std::array<double,3>&>::value << "\n";
+
+	std::array<int,5> a;
+	std::cout << a.size() << "\n";
+
+	double lim = 5. * 1./sqrt(2.);
+	std::array<double,3> x_min = {-lim,-lim,-lim};
+	std::array<double,3> x_max = {lim,lim,lim};
+	double eps_abs = 0.0001;
+	double eps_rel = 0.0001;
+
+	std::cout << "gauss 3d = "
+	   << cub::make_cubature_int<decltype(f)*,1,3>(
+			   f,10000,x_min,x_max,eps_abs,eps_rel)[0][0] << "\n";
 	
 	return 0;
 }
